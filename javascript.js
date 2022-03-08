40d6b7855a1535cafd2c7d8cafb8f7021d7ca939
Amit-Yadav21 <amit21@navgurukul.org>
2022-03-08 17:50:18 +0530

    This is my first test

---
+++ javascript.js
@@ -0,0 +1,45 @@
+
+
+
+Q.1 what is jascript ? 
+    - lava script is a scripting language ?
+Q.2 Describe what is 'var','let','const' ?
+    - var and let is same work on js . var is a globle variable and let is a local variable . const variable don't change the variable value .
+
+Q.3 In which category of datatype do the fpllowing go ?
+    - b) object , Array 
+
+Q.4(a) Perform the following steps by your code ?
+1) create an empty array .
+const arr = []
+console.log(arr);
+
+2) 
+const myf = [];   
+myf.push['amit'];
+myf.push['kumar'];
+myf.push['yadav'];
+console .log(myf);
+
+(b)(1)
+
+var = {}
+
+(2) var amit = {};
+    amit['name']='amit';
+    amit['Last Name']='yadav';
+    amit['Age']='24';
+    amit['Native place']='Himanchal';
+    amit['Agenda']='siftwwre devloper';
+    console.log(amit);
+(3) var yadav =['amit','kumar','yadav'];
+    var yadav1 = {};
+    yadav1['other'] = yadav;
+    console.log(yadav1);
+
+Q.5 
+    let amit = ['amit','yadav','kumar'];
+    let amit1 = [2,3,4,6,];
+    var add = amit.console(amit1);
+    console.log(add);
+
